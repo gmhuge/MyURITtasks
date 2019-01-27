173d91a73fd82bffc89aac8a23303b7be07da6ad
gmhuge <gmhuge@gmail.com>
2019-01-28 00:31:58 +0300

    new

--- app/src/main/java/com/example/android/myurittasks/MainActivity.java
+++ app/src/main/java/com/example/android/myurittasks/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.android.myurittasks;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
@@ -10,6 +16,8 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -21,23 +29,41 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    public static final int NEW_WORD_ACTIVITY_REQUEST_CODE = 1;
+
+    private WordViewModel mWordViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mWordViewModel = ViewModelProviders.of(this).get(WordViewModel.class);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+       //         Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+       //                 .setAction("Action", null).show();
+
+
+
+                    Intent intent = new Intent(MainActivity.this, NewWordActivity.class);
+                    startActivityForResult(intent, NEW_WORD_ACTIVITY_REQUEST_CODE);
+
+
             }
         });
 
@@ -58,8 +84,8 @@ public class MainActivity extends AppCompatActivity
         //CategoryAdapter adapter = new CategoryAdapter(this, getSupportFragmentManager(), categories);
         //viewPager.setAdapter(adapter);
 
-        SampleFragmentPagerAdapter adapter = new SampleFragmentPagerAdapter(getSupportFragmentManager(),getApplicationContext());
-        viewPager.setAdapter(adapter);
+        SampleFragmentPagerAdapter adapter2 = new SampleFragmentPagerAdapter(getSupportFragmentManager(),getApplicationContext());
+        viewPager.setAdapter(adapter2);
 
   //      viewPager.setAdapter(
   //              new SampleFragmentPagerAdapter(getSupportFragmentManager(), MainActivity.this));
@@ -71,6 +97,31 @@ public class MainActivity extends AppCompatActivity
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
         tabLayout.setSelectedTabIndicatorHeight(20);
 
+        /*
+
+//        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+
+        View view = inflater.inflate(R.layout.task_page, container, false);
+        TextView textView = (TextView) view;
+
+        final WordListAdapter adapter = new WordListAdapter(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Get a new or existing ViewModel from the ViewModelProvider.
+        mWordViewModel = ViewModelProviders.of(this).get(WordViewModel.class);
+
+        // Add an observer on the LiveData returned by getAlphabetizedWords.
+        // The onChanged() method fires when the observed data changes and the activity is
+        // in the foreground.
+        mWordViewModel.getAllWords().observe(this, new Observer<List<Word>>() {
+            @Override
+            public void onChanged(@Nullable final List<Word> words) {
+                // Update the cached copy of the words in the adapter.
+                adapter.setWords(words);
+            }
+        });
+*/
 
        // setContentView(R.layout.activity_main);
     }
@@ -131,4 +182,30 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == NEW_WORD_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+            Word word = new Word(data.getStringExtra(NewWordActivity.EXTRA_REPLY));
+            mWordViewModel.insert(word);
+        } else {
+            Toast.makeText(
+                    getApplicationContext(),
+                    R.string.empty_not_saved,
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Entity
+    public class Employee {
+
+        @PrimaryKey
+        public long id;
+
+        public String name;
+
+        public int salary;
+    }
+
 }
--- app/src/main/java/com/example/android/myurittasks/PageFragment.java
+++ app/src/main/java/com/example/android/myurittasks/PageFragment.java
@@ -1,17 +1,25 @@
 package com.example.android.myurittasks;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class PageFragment extends Fragment {
     public static final String ARG_PAGE = "ARG_PAGE";
 
     private int mPage;
+    private WordViewModel mWordViewModel;
 
     public static PageFragment newInstance(int page) {
         Bundle args = new Bundle();
@@ -30,9 +38,34 @@ public class PageFragment extends Fragment {
 
     @Override public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                                        Bundle savedInstanceState) {
+      //  View view = inflater.inflate(R.layout.task_page, container, false);
+           //  TextView textView = (TextView) view;
+           //   textView.setText("Fragment #" + mPage);
+
+        //        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+
         View view = inflater.inflate(R.layout.task_page, container, false);
-             TextView textView = (TextView) view;
-              textView.setText("Fragment #" + mPage);
+        RecyclerView recyclerView = (RecyclerView) view;
+
+        final WordListAdapter adapter = new WordListAdapter(getContext());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        // Get a new or existing ViewModel from the ViewModelProvider.
+        mWordViewModel = ViewModelProviders.of(this).get(WordViewModel.class);
+
+        // Add an observer on the LiveData returned by getAlphabetizedWords.
+        // The onChanged() method fires when the observed data changes and the activity is
+        // in the foreground.
+        mWordViewModel.getAllWords().observe(this, new Observer<List<Word>>() {
+            @Override
+            public void onChanged(@Nullable final List<Word> words) {
+                // Update the cached copy of the words in the adapter.
+                adapter.setWords(words);
+            }
+        });
+
+
         return view;
     }
 }
